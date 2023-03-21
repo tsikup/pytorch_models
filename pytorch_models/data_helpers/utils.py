681cd1de360a26d96b8f5d1d5bb2e6348ce5edd7
Nikos Tsiknakis <tsiknakisn@gmail.com>
2023-03-21 18:09:22 +0100

    fix bug for calculating standardization stats over batch of images

--- pytorch_models/data_helpers/utils.py
+++ pytorch_models/data_helpers/utils.py
@@ -16,16 +16,18 @@ def calculate_mean_and_std(channels_sum, channels_squared_sum, num_batches):
 def get_channels_sums_from_ndarray(
     data: np.ndarray, channels_last=False, max_value=255.0
 ):
+    aggregate = False
     if len(data.shape) == 3:
         if channels_last:
             channels = (0, 1)
         else:
             channels = (1, 2)
     elif len(data.shape) == 4:
+        aggregate = True
         if channels_last:
-            channels = (0, 1, 2)
+            channels = (1, 2)
         else:
-            channels = (0, 2, 3)
+            channels = (2, 3)
 
     if data.dtype == np.uint8:
         data = data.astype(np.float32)
@@ -33,6 +35,11 @@ def get_channels_sums_from_ndarray(
     # Mean over batch, height and width, but not over the channels
     channels_sums = data.mean(axis=channels)
     channels_squared_sum = (data**2).mean(axis=channels)
+
+    if aggregate:
+        channels_sums = channels_sums.sum(axis=0)
+        channels_squared_sum = channels_squared_sum.sum(axis=0)
+
     return channels_sums, channels_squared_sum
 
 
