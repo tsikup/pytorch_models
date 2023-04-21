31049ad04672323d38d88a2cd7df8b486638a708
Nikos Tsiknakis <tsiknakisn@gmail.com>
2023-04-21 13:33:02 +0300

    PredMIL and FeatureMIL

--- pytorch_models/models/classification/mil.py
+++ pytorch_models/models/classification/mil.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from ..base import BaseMILModel
+from pytorch_models.models.base import BaseMILModel
 
 
 def initialize_weights(module):
@@ -21,16 +21,117 @@ def initialize_weights(module):
             nn.init.constant_(m.bias, 0)
 
 
-class MIL(nn.Module):
+class PredMIL(nn.Module):
     def __init__(
         self,
-        size=[1024, 512],
+        size=None,
+        dropout=False,
+        n_classes=1,
+        aggregate: Union[str, float] = "mean",
+        agg_level="probs",
+        top_k: int = None,
+    ):
+        super(PredMIL, self).__init__()
+        if size is None:
+            size = [1024, 512]
+
+        self.size = size
+        self.top_k = top_k if top_k is not None else 0
+        self.n_classes = n_classes
+        self.aggregate = aggregate
+        self.agg_level = agg_level
+
+        if isinstance(self.aggregate, float):
+            assert 0.0 < self.aggregate < 1.0, "Aggregate must be between 0 and 1"
+
+        _classifier = list()
+
+        for i in range(len(self.size) - 1):
+            fc = [nn.Linear(self.size[i], self.size[i + 1]), nn.ReLU()]
+            if dropout:
+                fc.append(nn.Dropout(0.25))
+            _classifier.extend(fc)
+
+        _classifier.append(nn.Linear(size[-1], n_classes))
+        self.classifier = nn.Sequential(*_classifier)
+
+        initialize_weights(self)
+
+    def forward(self, x: List[torch.Tensor], return_features=False):
+        assert isinstance(x, list), (
+            "Please use `FeatureDatasetHDF5.collate` function to load dataset "
+            "because each sample may have different number of features."
+        )
+
+        batch_logits = []
+        batch_probs = []
+        features_list = []
+
+        for h in x:
+            logits = self.classifier(h)  # K x n_classes
+            probs = F.softmax(logits, dim=1) if self.n_classes > 1 else logits.sigmoid()
+
+            if self.top_k != 0:
+                if self.agg_level == "logits":
+                    top_instance_idx = logits.topk(
+                        self.top_k, dim=0, largest=self.top_k > 0, sorted=False
+                    )[1].squeeze()
+                else:
+                    top_instance_idx = probs.topk(
+                        self.top_k, dim=0, largest=self.top_k > 0, sorted=False
+                    )[1].squeeze()
+                logits = torch.index_select(logits, dim=0, index=top_instance_idx)
+                probs = torch.index_select(probs, dim=0, index=top_instance_idx)
+
+            if self.aggregate == "mean":
+                logits = torch.mean(logits, dim=0)
+                probs = torch.mean(probs, dim=0)
+            elif self.aggregate == "max":
+                logits = torch.max(logits, dim=0)[0]
+                probs = torch.max(probs, dim=0)[0]
+            elif self.aggregate == "min":
+                logits = torch.min(logits, dim=0)[0]
+                probs = torch.min(probs, dim=0)[0]
+            elif isinstance(self.aggregate, float):
+                logits = logits.kthvalue(
+                    int(logits.shape[0] * self.aggregate), dim=0, keepdim=False
+                )[0]
+                probs = probs.kthvalue(
+                    int(probs.shape[0] * self.aggregate), dim=0, keepdim=False
+                )[0]
+            else:
+                raise KeyError(f"Aggregate function `{self.aggregate}` not known.")
+
+            batch_logits.append(logits)
+            batch_probs.append(probs)
+
+            if return_features and self.top_k != 0:
+                top_features = torch.index_select(h, dim=0, index=top_instance_idx)
+            else:
+                top_features = h
+            features_list.append(top_features)
+
+        logits = torch.vstack(batch_logits)
+        probs = torch.vstack(batch_probs)
+        if return_features:
+            results_dict = dict(features=torch.stack(features_list, dim=0))
+            return logits, probs, results_dict
+        return logits, probs
+
+
+class FeatureMIL(nn.Module):
+    def __init__(
+        self,
+        size=None,
         dropout=False,
         n_classes=1,
         aggregates: Union[str, List[str]] = "mean",
         top_k=1,
     ):
-        super(MIL, self).__init__()
+        super(FeatureMIL, self).__init__()
+        if size is None:
+            size = [1024, 512]
+
         self.size = size
         self.top_k = top_k
         self.n_classes = n_classes
@@ -112,9 +213,7 @@ class MIL(nn.Module):
 
             if self.n_classes == 1:
                 y_probs = logits.sigmoid()
-                top_instance_idx = torch.topk(y_probs, self.top_k, dim=0)[1].view(
-                    1,
-                )
+                top_instance_idx = torch.topk(y_probs, self.top_k, dim=0)[1].squeeze()
                 top_instance_logits = torch.index_select(
                     logits, dim=0, index=top_instance_idx
                 )
@@ -152,7 +251,7 @@ class MIL(nn.Module):
             Y_prob = torch.vstack(batch_Y_prob)
             Y_hat = torch.vstack(batch_Y_hat)
             y_probs = torch.vstack(batch_y_probs)
-            results_dict = dict(results_dict=torch.vstack(batch_results_dict))
+            results_dict = dict(features=torch.vstack(batch_results_dict))
             return top_instance_logits, Y_prob, Y_hat, y_probs, results_dict
 
 
@@ -326,7 +425,7 @@ class MIL_PL(BaseMILModel):
                     top_k=self.top_k,
                 )
         else:
-            self.model = MIL(
+            self.model = FeatureMIL(
                 size=self.size,
                 dropout=self.dropout,
                 n_classes=self.n_classes,
@@ -361,3 +460,31 @@ class MIL_PL(BaseMILModel):
 
     def _forward(self, x):
         return self.model.forward(x)
+
+
+if __name__ == "__main__":
+    # create test data and model
+    batch = 32
+    n_features = 384
+    n_classes = 1
+    n_samples = 100
+
+    features = [torch.rand(n_samples, n_features) for _ in range(batch)]
+
+    for aggregate in ["mean", "max", "min", 0.25, 0.5, 0.75]:
+        model = PredMIL(
+            size=[384, 256, 128],
+            dropout=True,
+            n_classes=1,
+            aggregate=aggregate,
+            agg_level="probs",
+            top_k=10,
+        )
+
+        # test forward pass
+        logits, probs, results_dict = model.forward(features, return_features=True)
+        print("------------------------")
+        print("aggregate:", aggregate)
+        print(logits.shape)
+        print(probs.shape)
+        print(results_dict["features"].shape)
