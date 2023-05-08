fda5004fa921c7dd79e52f5dd7df91a9e7e13f6e
Nikos Tsiknakis <tsiknakisn@gmail.com>
2023-05-08 16:54:22 +0200

    fix

--- pytorch_models/models/classification/transmil.py
+++ pytorch_models/models/classification/transmil.py
@@ -50,12 +50,19 @@ class TransMIL(nn.Module):
     def __init__(self, n_classes, size=(1024, 512)):
         super(TransMIL, self).__init__()
 
+        if len(size) > 2:
+            p_size = size
+            size = size[-2:]
+        else:
+            p_size = size
+
         self._fc1 = nn.Sequential(
             *[
-                nn.Sequential(nn.Linear(size[i], size[i + 1]), nn.ReLU())
-                for i in range(len(size) - 1)
+                nn.Sequential(nn.Linear(p_size[i], p_size[i + 1]), nn.ReLU())
+                for i in range(len(p_size) - 1)
             ]
         )
+
         self.pos_layer = PPEG(dim=size[1])
         self.cls_token = nn.Parameter(torch.randn(1, 1, size[1]))
         self.n_classes = n_classes
