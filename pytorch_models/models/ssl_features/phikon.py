102d894b07941e07bf40b96410f2044d9707c53c
Nikos Tsiknakis <tsiknakisn@gmail.com>
2024-03-12 11:35:09 +0200

    update phikon to return only features

--- pytorch_models/models/ssl_features/phikon.py
+++ pytorch_models/models/ssl_features/phikon.py
@@ -1,6 +1,17 @@
+import torch
 from transformers import ViTModel
 
 
+class PhiKonModel(torch.nn.Module):
+    def __init__(self):
+        super(PhiKonModel, self).__init__()
+        self.model = ViTModel.from_pretrained("owkin/phikon", add_pooling_layer=False)
+
+    def forward(self, x):
+        outputs = self.model(x)
+        features = outputs.last_hidden_state[:, 0, :]  # (1, 768) shape
+        return features
+
+
 def phikon():
-    model = ViTModel.from_pretrained("owkin/phikon", add_pooling_layer=False)
-    return model
+    return PhiKonModel()
