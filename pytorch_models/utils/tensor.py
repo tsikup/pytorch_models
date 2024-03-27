6f6be9cb17d9880e1ac92fb3aef2c104a05aafe1
Nikos Tsiknakis <tsiknakisn@gmail.com>
2024-03-27 15:02:27 +0200

    pad_col implementation as in pycox

--- pytorch_models/utils/tensor.py
+++ pytorch_models/utils/tensor.py
@@ -9,6 +9,20 @@ import torchvision.transforms.functional as F
 from torch import Tensor
 
 
+def pad_col(input, val=0, where="end"):
+    """Addes a column of `val` at the start of end of `input`."""
+    if len(input.shape) != 2:
+        raise ValueError(f"Only works for `input` tensor that is 2-D.")
+    pad = torch.zeros_like(input[:, :1])
+    if val != 0:
+        pad = pad + val
+    if where == "end":
+        return torch.cat([input, pad], dim=1)
+    elif where == "start":
+        return torch.cat([pad, input], dim=1)
+    raise ValueError(f"Need `where` to be 'start' or 'end', got {where}")
+
+
 def kronecker_product_einsum_batched(A: torch.Tensor, B: torch.Tensor):
     """
     Batched Version of Kronecker Products
