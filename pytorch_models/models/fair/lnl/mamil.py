0f6f4517d836b7b132f67db0f2ffa7bbf535697b
Nikos Tsiknakis <tsiknakisn@gmail.com>
2024-05-08 10:41:43 +0200

    arg: lambda_aux

--- pytorch_models/models/fair/lnl/mamil.py
+++ pytorch_models/models/fair/lnl/mamil.py
@@ -33,6 +33,7 @@ class MAMIL_LNL_PL(BaseMILModel_LNL):
         config: DotMap,
         n_classes: int,
         n_groups: int,
+        aux_lambda=0.1,
         size: Union[List[int], Tuple[int, int]] = None,
         dropout: bool = True,
         multires_aggregation: Union[None, str] = None,
@@ -47,6 +48,7 @@ class MAMIL_LNL_PL(BaseMILModel_LNL):
             config,
             n_classes=n_classes,
             n_groups=n_groups,
+            aux_lambda=aux_lambda,
             multires_aggregation=multires_aggregation,
             size=size,
             gradient_clip_value=gradient_clip_value,
@@ -61,51 +63,3 @@ class MAMIL_LNL_PL(BaseMILModel_LNL):
         self.model = MultiAttentionMIL_LNL(
             self.n_classes, size, use_dropout=self.dropout
         )
-
-
-if __name__ == "__main__":
-    import torch
-
-    config = DotMap(
-        {
-            "num_classes": 1,
-            "model": {
-                "input_shape": 256,
-                "classifier": "minet_ds",
-            },
-            "trainer": {
-                "optimizer_params": {"lr": 1e-3},
-                "batch_size": 1,
-                "loss": ["ce"],
-                "classes_loss_weights": None,
-                "multi_loss_weights": None,
-                "samples_per_class": None,
-                "sync_dist": False,
-                "l1_reg_weight": None,
-                "l2_reg_weight": None,
-            },
-            "devices": {
-                "nodes": 1,
-                "gpus": 1,
-            },
-            "metrics": {"threshold": 0.5},
-        }
-    )
-
-    x = torch.rand(10, 384)
-    y = torch.randint(0, 2, [32, 1])
-    y_aux = torch.randint(0, 2, [32, 1])
-
-    data = dict(
-        features=[dict(target=x) for _ in range(32)],
-        labels=y,
-        labels_group=y_aux,
-        slide_name="tmp",
-    )
-
-    model = MAMIL_LNL_PL(config, n_classes=1, n_groups=1, size=[384, 256, 128, 64])
-
-    o = model.forward(data, is_adv=True)
-    o2 = model.forward(data, is_adv=False)
-
-    print(o)
