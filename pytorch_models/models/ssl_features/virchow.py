e115064851b6745604afc4975662c6996a472846
Nikos Tsiknakis <32893089+tsikup@users.noreply.github.com>
2024-11-19 14:14:42 +0100

    init virchow2 model

---
+++ pytorch_models/models/ssl_features/virchow.py
@@ -0,0 +1,46 @@
+"""
+https://github.com/owkin/HistoSSLscaling/tree/main
+"""
+
+import os
+import torch
+import timm
+from timm.layers import SwiGLUPacked
+from timm.data import resolve_data_config
+from timm.data.transforms_factory import create_transform
+
+
+class VirchowModel(torch.nn.Module):
+    def __init__(self, ckpt_dir):
+        super(VirchowModel, self).__init__()
+        # need to specify MLP layer and activation function for proper init
+        self.model = timm.create_model(
+            "hf-hub:paige-ai/Virchow2",
+            pretrained=True,
+            mlp_layer=SwiGLUPacked,
+            act_layer=torch.nn.SiLU,
+        )
+
+    def get_transforms(self):
+        transforms = create_transform(
+            **resolve_data_config(self.model.pretrained_cfg, model=self.model)
+        )
+        return transforms
+
+    def forward(self, x):
+        output = self.model(x)  # size: 1 x 261 x 1280
+
+        class_token = output[:, 0]  # size: 1 x 1280
+        patch_tokens = output[
+            :, 5:
+        ]  # size: 1 x 256 x 1280, tokens 1-4 are register tokens so we ignore those
+
+        # concatenate class token and average pool of patch tokens
+        embedding = torch.cat(
+            [class_token, patch_tokens.mean(1)], dim=-1
+        )  # size: 1 x 2560
+        return embedding
+
+
+def virchow():
+    return VirchowModel()
